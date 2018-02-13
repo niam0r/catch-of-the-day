fa6cbee39fb8584b81755f68d7738249a92efb5c
Romain niam0r <niamor@mm.st>
2018-02-13 13:59:53 +0000

    ready for gh pages deploy

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import NotFound from './components/NotFound';
 
 const Root = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/catch-of-the-day/">
       <div>
         <Match exactly pattern="/" component={StorePicker} />
         <Match pattern="/store/:storeId" component={App} />
